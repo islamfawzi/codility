d7de3849546b5fef39c2ba4e7d640d45492b2c07
islam <islam.fawzi99@gmail.com>
2016-05-01 14:33:24 +0300

    commit all project files

---
+++ src/com/TimeComplexity/FrogJmp.java
@@ -0,0 +1,28 @@
+package com.TimeComplexity;
+
+public class FrogJmp {
+
+    public int solution(int X, int Y, int D){
+  		
+    	int count = 0;
+    	if(X < Y){
+	    	int Jmp = X;
+	    	
+	    	while(Jmp < Y){
+	    		Jmp += D;
+	    		count++;
+	    	}
+    	}
+    	
+		return count;
+	}
+
+	public static void main(String[] args) {
+        
+	 
+		System.out.println(new FrogJmp().solution(20, 85, 40));
+	}
+	
+	
+
+}
